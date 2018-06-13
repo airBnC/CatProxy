47f5c0792d1bbcc2d5f1a12e353168b7aabcac47
Catherine Ligad <kateligad@gmail.com>
2018-06-13 16:30:16 -0700

    finished proxy server setup

--- cat-proxy-server/server/index.js
+++ cat-proxy-server/server/index.js
@@ -5,7 +5,33 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/rooms', express.static(path.join(__dirname, '../public')));
+app.use('/rooms/:number', express.static(path.join(__dirname, '../public')));
+
+
+app.get('/api/rooms/:number/bookingInfo/', (req, res) => {
+  const {number} = req.params;
+  res.redirect(`http://127.0.0.1:3002/api/rooms/${number}/bookingInfo/`);
+});
+
+app.get('/api/listings/:number/reviews/', (req, res) => {
+  const {number} = req.params;
+  res.redirect(`http://127.0.0.1:3004/api/listings/${number}/reviews/`);
+});
+app.get('/api/listings/:number/averagestars/', (req, res) => {
+  const {number} = req.params;
+  res.redirect(`http://127.0.0.1:3004/api/listings/${number}/averagestars/`);
+});
+
+app.get('/api/rooms/:number/general/', (req, res) => {
+  const {number} = req.params;
+  res.redirect(`http://127.0.0.1:3003/api/rooms/${number}/general/`);
+});
+app.get('/api/rooms/:number/amenities/', (req, res) => {
+  const {number} = req.params;
+  res.redirect(`http://127.0.0.1:3003/api/rooms/${number}/amenities/`);
+});
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
